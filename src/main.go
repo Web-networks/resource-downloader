843767b2f1b6bc7e518f6a9a179a329e3794f746
Albert Sverdlov <sverdlov@joom.com>
2020-04-24 18:43:04 +0300

    Implement resource download from s3

--- src/main.go
+++ src/main.go
@@ -1,12 +1,81 @@
 package main
 
-import "os"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path"
 
-const containerDir = "/neuroide"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/mkideal/cli"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+)
+
+type argT struct {
+	cli.Helper
+	OutputDir string `cli:"output_dir" usage:"output directory" dft:"/neuroide"`
+
+	ModelBucket string `cli:"model_bucket" usage:"model s3 bucket"`
+	ModelPath string `cli:"model_path" usage:"model s3 path"`
+
+	UserInputBucket string `cli:"user_input_bucket" usage:"user-input s3 bucket"`
+	UserInputPath string `cli:"user_input_path" usage:"user-input s3 path"`
+}
+
+func download(downloader *s3manager.Downloader, bucket, item string, outputFile *os.File) error {
+	numBytes, err := downloader.Download(outputFile,
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(item),
+		})
+	if err != nil {
+		return errors.New(fmt.Sprintf("Unable to download item %q, %v", item, err))
+	}
+
+	fmt.Println("Downloaded", outputFile.Name(), numBytes, "bytes")
+	return nil
+}
+
+func Download(args *argT) error {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-central-1"),
+		Credentials: credentials.NewEnvCredentials(),
+	})
+	if err != nil {
+		return err
+	}
+	downloader := s3manager.NewDownloader(sess)
+
+	files := []struct {
+		Bucket string
+		Path string
+	}{
+		{ Bucket: args.ModelBucket, Path: args.ModelPath },
+		{ Bucket: args.UserInputBucket, Path: args.UserInputPath },
+	}
+
+	for _, f := range files {
+		if f.Bucket == "" || f.Path == "" {
+			continue
+		}
+		outputFile, err := os.Create(path.Join(args.OutputDir, f.Path))
+		if err != nil {
+			return err
+		}
+		if err := download(downloader, f.Bucket, f.Path, outputFile); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
 
 func main() {
-	_, err := os.Create(containerDir + "/test")
-	if err != nil {
-		panic(err)
-	}
+	os.Exit(cli.Run(new(argT), func(ctx *cli.Context) error {
+		argv := ctx.Argv().(*argT)
+		return Download(argv)
+	}))
 }
